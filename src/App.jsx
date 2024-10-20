e416b24b5345c21f4f6926246e458019dce124fb
Purbanka <purbankaghosh1708@gmail.com>
2024-10-20 23:43:27 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let[counter,setCounter]=useState(15)
+
+  // let counter=15
+
+  const addValue=()=>{
+   
+   setCounter(counter+1)
+    
+  }
+
+  const removeValue=()=>{
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+     <h1>Purbanka</h1>
+     <h2>counter value:{counter}</h2>
+
+     <button onClick={addValue()}>Add value {counter}</button>
+     <br/>
+     <button onClick={removeValue()}>Remove value {counter}</button>
+    <p>footer:{counter}</p> 
+    </>
+  )
+}
+
+export default App
